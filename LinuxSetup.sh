a1a56ebd830e8232a547fba46f60d0d1d4b0e9c4
Jason Antic <jsa169@gmail.com>
2018-05-29 21:28:43 -0700

    Adding dog breed notebook;  Adding LinuxSetup
     Adding dog breed notebook;  Adding LinuxSetup

---
+++ LinuxSetup.sh
@@ -0,0 +1,2 @@
+sudo ln -sf "/media/jason/Projects/Deep Learning/Fast.AI 2018/fastai" "/media/jason/Projects/Deep Learning/Fast.AI 2018/courses/dl1/fastai"
+sudo ln -sf "/media/jason/Projects/Deep Learning/Fast.AI 2018/fastai" "/media/jason/Projects/Deep Learning/Fast.AI 2018/courses/dl2/fastai"
